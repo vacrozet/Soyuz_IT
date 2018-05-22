8a640ee69fb9d44e0119209fbc9d8add7ad627f0
vacrozet <vacrozet@student.42.fr>
2018-05-22 13:00:13 -0300

    liste society

--- app/controller/society/deleteSociety.js
+++ app/controller/society/deleteSociety.js
@@ -13,6 +13,7 @@ module.exports = (req, res) => {
   if (req.params.id !== '') {
     db.get().then((db) => {
       db.collection('Society').deleteOne({_id: req.params.id})
+      db.collection('Users').remove({idSociety: req.params.id})
       return response(res, 200, true, 'Société supprimer')
     })
   } else return response(res, 400, false, 'Champ non renseigné')
